5cad8ab288829cc7f70d6dd2afbda7dc7e6e5c7a
みよししゅん <miyoshishun@miyoshishunnoMacBook-Air.local>
2019-12-31 17:30:35 +0900

    2019/12/31 パーミッションの変更 細かい修正

--- script/Docker_Log_Copy.sh
+++ script/Docker_Log_Copy.sh
@@ -12,41 +12,48 @@
 source /home/ec2-user/infra_Script/conf/Env_Valiable.conf
 
 Script_Name=Docker_Log_Copy
-Scr_Local_Conf=${script_conf}/${Script_Name}.conf
+Script_Local_Conf=${script_conf}/${Script_Name}.conf
 Script_Local_Log=${script_log}/`date '+%Y%m%d'`_${Script_Name}.log
 
 #===================
 #start_proc
 #===================
+#logの作成
 touch ${Script_Local_Log}
 
 
 #===================
 #copy_proc
 #===================
+#dockerのプロセス確認
 dockerPSdata=${script_tmp}/dockerPS.csv
 echo "start :${Script_Name}.sh" >　2>&1 >> ${Script_Local_Log}
-
 docker ps >> ${dockerPSdata}
+
+#dockerのプロセス確認のconfの読み取り
 while read -a line
 do
+
+  #Docker_Log_Copyのconfファイルの読み取り
   while read -a line2
   do
     if [[ "${line[1]}" = "${line2[0]}" ]]
     then
+      #dockerからログをコピーする
       echo "${line2[0]}_${line2[3]}_LogCopy" >　2>&1 >> ${Script_Local_Log}
       docker cp ${line[0]}:${line2[1]}/${line2[2]} ${script_tmp}/ >　2>&1 >> ${Script_Local_Log}
 
-
-      if [ -e ${Script_log}/${line2[0]} ]
+      #dockerからコピーしたファイルを別の場所に移す
+      if [ -e ${script_log}/${line2[0]} ]
       then
         mv ${script_tmp}/${line2[2]}/${line2[3]}* ${script_log}/${line2[0]} >　2>&1 >> ${Script_Local_Log}
       else
+        echo "not Dir"
         mkdir ${script_log}/${line2[0]} >　2>&1 >> ${Script_Local_Log}
         mv ${script_tmp}/${line2[2]}/${line2[3]}* ${script_log}/${line2[0]} >　2>&1 >> ${Script_Local_Log}
       fi
     fi
-  done<${Scr_Local_Conf}
+  done<${Script_Local_Conf}
 done<${dockerPSdata}
 
 #===================
--- script/Log_Copy2S3.sh
+++ script/Log_Copy2S3.sh
@@ -14,6 +14,8 @@ Script_Name=Log_Copy2S3
 Script_Local_Conf=${script_conf}/${Script_Name}.conf
 Script_Local_Log=${script_log}/`date '+%Y%m%d'`_${Script_Name}.log
 
+s3bucket=//tapino2logbucket
+
 #===================
 #start_proc
 #===================
@@ -25,10 +27,19 @@ touch ${Script_Local_Log}
 echo "start :${Script_Name}.sh" >　2>&1 >> ${Script_Local_Log}
 while read -a line
 do
-  
+  if [ -e ${script_log}/${line[0]} ]
+  then
+    ls ${script_log}/${line[0]} > ${script_tmp}/${line[0]}.tmp  >　2>&1 >> ${Script_Local_Log}
 
+    while read -a line2
+    do
+      aws s3 mv ${script_log}/${line[0]}/${line2[0]} s3:${s3bucket}/${line[0]}/  >　2>&1 >> ${Script_Local_Log}
+      rm -f ${script_log}/${line[0]}/${line2[0]}  >　2>&1 >> ${Script_Local_Log}
+    done<${script_tmp}/${line[0]}.tmp
 
-done<${Scr_Local_Conf}
+    rm -f ${script_tmp}/${line[0]}.tmp
+  fi
+done<${Script_Local_Conf}
 
 #===================
 #end_Proc
